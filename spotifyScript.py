6bc4824bc73838ff50ec03e3b058e5b180347369
linboi <tedblonde@gmail.com>
2021-05-30 19:25:40 +0100

    Add ability to analyse multiple playlists at once

--- spotifyScript.py
+++ spotifyScript.py
@@ -12,23 +12,29 @@ for idx, item in enumerate(results['items']):
 	track = item['track']
 	print(idx, track['artists'][0]['name'], " – ", track['name'])
 
-# Analyse a specific playlist
-playlist = sp.playlist_tracks("08r7gmYnqqZ7YBih2kyug2", limit=100)
-i = 0
+playlistList = ["08r7gmYnqqZ7YBih2kyug2"]
+
 artistCounts = {}
 artistLengths = {}
 totalLength = 0
-while i < playlist['total']:
-	playlist = sp.playlist_tracks("08r7gmYnqqZ7YBih2kyug2", limit=100, offset=i)
-	for track in playlist['items']:
-		i += 1
-		totalLength += track['track']['duration_ms']
-		if track['track']['artists'][0]['name'] in artistCounts:
-			artistCounts[track['track']['artists'][0]['name']] += 1
-			artistLengths[track['track']['artists'][0]['name']] += track['track']['duration_ms']
-		else:
-			artistCounts[track['track']['artists'][0]['name']] = 1
-			artistLengths[track['track']['artists'][0]['name']] = track['track']['duration_ms']
+songCount = 0
+for thisPlaylist in playlistList:
+# Analyse a specific playlist
+	playlist = sp.playlist_tracks(thisPlaylist, limit=100)
+
+	i = 0
+	while i < playlist['total']:
+		playlist = sp.playlist_tracks(thisPlaylist, limit=100, offset=i)
+		for track in playlist['items']:
+			songCount += 1
+			i += 1
+			totalLength += track['track']['duration_ms']
+			if track['track']['artists'][0]['name'] in artistCounts:
+				artistCounts[track['track']['artists'][0]['name']] += 1
+				artistLengths[track['track']['artists'][0]['name']] += track['track']['duration_ms']
+			else:
+				artistCounts[track['track']['artists'][0]['name']] = 1
+				artistLengths[track['track']['artists'][0]['name']] = track['track']['duration_ms']
 
 # dicts are now filled with song and length totals, putting them into lists to be sorted
 
@@ -43,4 +49,4 @@ artistSongLengths.sort(key=lambda tup: tup[1], reverse=True)
 artistSongCount.sort(key=lambda tup: tup[1], reverse=True)
 print(totalLength)
 for idx, artist in enumerate(artistSongCount):
-	print((str(idx) + ". ").ljust(5, " ") + artist[0].ljust(50, "-") + " " +  str(round((artist[1]/i)*100, 3)) + "\t" + (str(idx) + ". ").ljust(5, " ") + artistSongLengths[idx][0].ljust(50, "-") + " " + str(round((artistSongLengths[idx][1]/totalLength)*100, 3)))
+	print((str(idx) + ". ").ljust(5, " ") + artist[0].ljust(50, "-") + " " +  str(round((artist[1]/songCount)*100, 3)) + "\t" + (str(idx) + ". ").ljust(5, " ") + artistSongLengths[idx][0].ljust(50, "-") + " " + str(round((artistSongLengths[idx][1]/totalLength)*100, 3)))
